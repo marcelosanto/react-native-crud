0c23c506d6b478575c5582154e0e3167ab004314
Marcelo Santos <rocha.mer21@gmail.com>
2020-09-24 23:19:42 -0300

    comencando o app

---
+++ src/data/Users.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Joaozinho Totovs',
+    email: 'jojo@jojo.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+  },
+  {
+    id: 2,
+    name: 'Monica Tericia',
+    email: 'monteri@uol.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png',
+  },
+  {
+    id: 3,
+    name: 'Jessica Toras',
+    email: 'jessica@jessica.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/04/25/21/14/woman-1353211_960_720.png',
+  },
+  {
+    id: 4,
+    name: 'Marcos Benicio',
+    email: 'marcos@marcos.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+  },
+  {
+    id: 5,
+    name: 'Ivan Duarte',
+    email: 'ivandua@ivandua.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png',
+  },
+]
---
+++ src/views/UserForm.js
@@ -0,0 +1,7 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import { Text } from 'react-native'
+
+export default (props) => {
+  return <Text>UserForm</Text>
+}
