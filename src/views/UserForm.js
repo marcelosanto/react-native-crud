89c65961b9d95419286f86f269cc4e56a6e0466d
Marcelo Santos <rocha.mer21@gmail.com>
2020-09-25 08:05:50 -0300

    editando a tela de form

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,7 +1,48 @@
 import { createStackNavigator } from '@react-navigation/stack'
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, TextInput, StyleSheet, Button } from 'react-native'
 
-export default (props) => {
-  return <Text>UserForm</Text>
+export default ({ route, navigation }) => {
+  const [user, setUser] = useState(route.params ? route.params : {})
+
+  return (
+    <View style={styles.form}>
+      <Text>Nome</Text>
+      <TextInput
+        onChangeText={(name) => setUser({ ...user, name })}
+        placeholder="Informa o Nome"
+        value={user.name}
+        style={styles.input}
+      />
+
+      <Text>Email</Text>
+      <TextInput
+        onChangeText={(email) => setUser({ ...user, email })}
+        placeholder="Informa o E-mail"
+        value={user.email}
+        style={styles.input}
+      />
+
+      <Text>URL do Avatar</Text>
+      <TextInput
+        onChangeText={(avatarUrl) => setUser({ ...user, avatarUrl })}
+        placeholder="Informa a URL do Avatar"
+        value={user.avatarUrl}
+        style={styles.input}
+      />
+      <Button title="Salvar" onPress={() => navigation.goBack()} />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+  },
+})
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,16 +1,46 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
+import { View, Text, FlatList, Alert } from 'react-native'
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements'
 
 import users from '../data/Users'
 
 export default (props) => {
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete ' + user.id)
+        },
+      },
+      { text: 'Não' },
+    ])
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    )
+  }
+
   function getUserItem({ item: user }) {
     return (
       <ListItem
         key={user.id}
         bottomDivider
-        onPress={() => props.navigation.navigate('UserForm')}>
+        onPress={() => props.navigation.navigate('UserForm', user)}
+        rightElement={getActions(user)}>
         <Avatar source={{ uri: user.avatarUrl }} />
         <ListItem.Content>
           <ListItem.Title>{user.name}</ListItem.Title>
