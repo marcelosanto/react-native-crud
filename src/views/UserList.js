0b18dc2310a459a37d1889686db4b020a3a21423
Marcelo Santos <rocha.mer21@gmail.com>
2020-09-25 08:17:42 -0300

    introduzindo o ContextApi

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, FlatList, Alert } from 'react-native'
 import { ListItem, Avatar, Button, Icon } from 'react-native-elements'
-
-import users from '../data/Users'
+import UsersContext from '../context/UsersContext'
 
 export default (props) => {
+  const { state } = useContext(UsersContext)
+
   function confirmUserDeletion(user) {
     Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
       {
@@ -54,7 +55,7 @@ export default (props) => {
     <View>
       <FlatList
         keyExtractor={(user) => user.id.toString()}
-        data={users}
+        data={state.users}
         renderItem={getUserItem}
       />
     </View>
